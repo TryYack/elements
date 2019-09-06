a56386c9d20407738a0122168309da454a2bb48c
Jo du Plessis <jo@joduplessis.com>
2019-09-06 11:52:46 +0200

    Button themes, sizing & outline

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,54 +1,107 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const Container = styled.div<{ size?: string }>`
-  width: ${props => {
-    if (props.size === "small") return "150";
-    if (props.size === "medium") return "200";
-    if (props.size === "large") return "250";
-    
-    return "200";
-  }}px;
+const Container = styled.div<{ jumbo?: boolean }>`
+  width: max-content;
   position: relative;
-  display: inline-block;
+  max-width: ${props => {
+    if (props.jumbo) return "300px";
+    return "200px";
+  }};
 `;
 
-const Inner = styled.div<{ size?: string }>`
-  background-color: #007af5;
-  color: white;
-  font-size: 25px;
-  font-weight: 600;
-  padding: ${props => {
-    if (props.size === "small") return "10px 20px 10px 20px";
-    if (props.size === "medium") return "15px 25px 15px 25px";
-    if (props.size === "large") return "20px 30px 20px 30px";
-    
-    return "15px 25px 15px 25px";
+const Inner = styled.div<{
+  jumbo?: boolean,
+  theme?: string,
+  outline?: boolean,
+}>`
+  height: ${props => {
+    if (props.jumbo) return "80px";
+    return "40px";
   }};
-  border-radius: 5px;
-  transition: background-color 0.25s, color 0.25s;
+  border-radius: ${props => {
+    if (props.jumbo) return "10px";
+    return "6px";
+  }};
+  background-color: ${props => {
+    if (props.theme == "muted") return "#F8F9FA";
+    if (props.theme == "danger") return "#F8B6B7";
+    if (props.theme == "blue") return "#EFF8FF";
+    return "#007af5";
+  }};
+  color: ${props => {
+    if (props.theme == "muted") return "#ADB5BD";
+    if (props.theme == "danger") return "#8E2F29";
+    if (props.theme == "blue") return "#007af5";
+    return "white";
+  }};
+  border: ${props => {
+    if (props.outline && !props.theme) return "1.5px solid #007af5";
+    if (props.outline && props.theme == "muted") return "1.5px solid #ADB5BD";
+    if (props.outline && props.theme == "danger") return "1.5px solid #8E2F29";
+    if (props.outline && props.theme == "blue") return "1.5px solid #286BA4";
+    return "none";
+  }};
+  box-sizing: border-box;
+  width: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  background-size: cover;
+  position: relative;
+  transition: background-color 0.25s, color 0.25s, border 0.25s;
   cursor: pointer;
-  text-align: center;
+
+  &:hover {
+    color: ${props => {
+      if (props.theme == "muted") return "#0f081f";
+      if (props.theme == "danger") return "white";
+      if (props.theme == "blue") return "white";
+      return "white";
+    }};
+    background-color: ${props => {
+      if (props.theme == "muted") return "#F1F3F5";
+      if (props.theme == "danger") return "#ED553B";
+      if (props.theme == "blue") return "#007af5";
+      return "#0f081f";
+    }};
+
+    border: ${props => {
+      if (props.outline && !props.theme) return "2px solid #0f081f";
+      if (props.outline && props.theme == "muted") return "2px solid #F1F3F5";
+      if (props.outline && props.theme == "danger") return "2px solid #ED553B";
+      if (props.outline && props.theme == "blue") return "2px solid #007af5";
+      return "none";
+    }};
+  }
+`;
+
+const Text = styled.span<{
+  jumbo?: boolean,
+  theme?: string,
+}>`
+  padding: ${props => {
+    if (props.jumbo) return "0px 30px 0px 30px";
+    return "0px 15px 0px 15px";
+  }};
+  font-weight: ${props => {
+    if (props.jumbo) return "500";
+    return "500";
+  }};
+  font-size: ${props => {
+    if (props.jumbo) return "24px";
+    return "12px";
+  }};
+  margin: 0px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
   font-family: -apple-system, BlinkMacSystemFont,
   "Segoe UI", "Roboto", "Oxygen",
   "Ubuntu", "Cantarell", "Fira Sans",
   "Droid Sans", "Helvetica Neue", sans-serif;
-  font-size: ${props => {
-    if (props.size === "small") return "8";
-    if (props.size === "medium") return "12";
-    if (props.size === "large") return "16";
-
-    return "12";
-  }}px;
-
-  &:hover {
-    background-color: #0f081f;
-    color: #007af5;
-  }
-
-  &:first-child {
-    margin-right: 5px;
-  }
 `;
 
 interface IButtonProps {
@@ -59,29 +112,47 @@ interface IButtonProps {
    */
   text: string;
 
-  /** 
-   * Possible string values:
-   * - "small"
-   * - "medium"
-   * - "large"
+  /**
+   * Colour theme for button
+   *
+   * @default " " (blue)
    */
-  size?: any;
+  theme?: string;
+
+  /**
+   * Possible values are true/false
+   *
+   * @default false
+   */
+  outline?: boolean;
+
+  /**
+   * Possible values are true/false
+   *
+   * @default false
+   */
+  jumbo?: boolean;
 
   /** Called when an empty cell is clicked. */
   onClick?: any;
 }
 
 /**
- * Button component. 
+ * Button component.
  */
 export const Button: React.FunctionComponent<IButtonProps> = (props: IButtonProps) => {
   return (
-    <Container size={props.size}>
+    <Container jumbo={props.jumbo}>
       <Inner
-        className="inner"
-        size={props.size}
-        onClick={props.onClick}>
-        {props.text}
+        outline={props.outline}
+        jumbo={props.jumbo}
+        theme={props.theme}>
+        <Text
+          theme={props.theme}
+          jumbo={props.jumbo}
+          onClick={props.onClick}>
+          {props.text}
+        </Text>
       </Inner>
     </Container>
   );
--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -6,19 +6,113 @@ import { withInfo } from "@storybook/addon-info";
 
 storiesOf('Button', module)
   .add(
-    'standard',
+    'Long text',
     withInfo({ inline: true })(() => (
       <Button
-        text="Click Me"
+        text="This is some long text being hidden"
         onClick={action('clicked')}
       />
-    )),
-    {
-      notes: {
-        markdown: `
-          # Button component
-          ## Any other resources about this component will be added here
-        `
-      },
-    }
-  );
+    )));
+
+  storiesOf('Button', module)
+    .add(
+      'Normal text',
+      withInfo({ inline: true })(() => (
+        <Button
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Muted coloring',
+      withInfo({ inline: true })(() => (
+        <Button
+          theme="muted"
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Blue coloring',
+      withInfo({ inline: true })(() => (
+        <Button
+          theme="blue"
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Danger coloring',
+      withInfo({ inline: true })(() => (
+        <Button
+          theme="danger"
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Bordered',
+      withInfo({ inline: true })(() => (
+        <Button
+          outline={true}
+          theme="danger"
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Jumbo',
+      withInfo({ inline: true })(() => (
+        <Button
+          jumbo={true}
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+  storiesOf('Button', module)
+    .add(
+      'Jumbo muted bordered',
+      withInfo({ inline: true })(() => (
+        <Button
+          jumbo={true}
+          outline ={true}
+          theme="muted"
+          text="Click Here"
+          onClick={action('clicked')}
+        />
+      )));
+
+    /*
+
+      Template:
+
+      storiesOf('Button', module)
+        .add(
+          'medium',
+          withInfo({ inline: true })(() => (
+            <Button
+              text="Click Me"
+              onClick={action('clicked')}
+            />
+          )),
+          {
+            notes: {
+              markdown: `
+                # Button component
+                ## Any other resources about this component will be added here
+              `
+            },
+          }
+        );
+    */
