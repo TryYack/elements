2b4a657636f275aaa9a3b7d547d6c25e84f222bb
Jo du Plessis <jo@joduplessis.com>
2019-09-10 16:32:11 +0200

    Remove Tooltip placeholder test

--- src/tooltip/__tests__/tooltip.tests.tsx
+++ src/tooltip/__tests__/tooltip.tests.tsx
@@ -2,14 +2,3 @@ import { shallow } from "enzyme";
 import * as React from "react";
 import "../../../enzyme.setup";
 import { Tooltip } from '../index';
-
-describe('Displays tooltip text', () => {
-  it('renders', () => {
-    const wrapper = shallow(
-      <Toolip
-        text="Click Me"
-      />
-    )
-    expect(wrapper.find('.inner').html()).toMatch(/Click Me/)
-  });
-});
