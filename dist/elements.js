dcdab3acc6131f4c4310dd402afcd41ee4627f02
Jo du Plessis <jo@joduplessis.com>
2019-11-04 14:43:39 +0200

    Production file build

--- dist/elements.js
+++ dist/elements.js
@@ -2152,100 +2152,6 @@ exports.default = _default;
 
 /***/ }),
 
-/***/ "./node_modules/@material-ui/icons/LockOutlined.js":
-/*!*********************************************************!*\
-  !*** ./node_modules/@material-ui/icons/LockOutlined.js ***!
-  \*********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireDefault(__webpack_require__(/*! react */ "react"));
-
-var _createSvgIcon = _interopRequireDefault(__webpack_require__(/*! ./utils/createSvgIcon */ "./node_modules/@material-ui/icons/utils/createSvgIcon.js"));
-
-var _default = (0, _createSvgIcon.default)(_react.default.createElement(_react.default.Fragment, null, _react.default.createElement("g", {
-  fill: "none"
-}, _react.default.createElement("path", {
-  d: "M0 0h24v24H0V0z"
-}), _react.default.createElement("path", {
-  d: "M0 0h24v24H0V0z",
-  opacity: ".87"
-})), _react.default.createElement("path", {
-  d: "M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"
-})), 'LockOutlined');
-
-exports.default = _default;
-
-/***/ }),
-
-/***/ "./node_modules/@material-ui/icons/MoreHorizOutlined.js":
-/*!**************************************************************!*\
-  !*** ./node_modules/@material-ui/icons/MoreHorizOutlined.js ***!
-  \**************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireDefault(__webpack_require__(/*! react */ "react"));
-
-var _createSvgIcon = _interopRequireDefault(__webpack_require__(/*! ./utils/createSvgIcon */ "./node_modules/@material-ui/icons/utils/createSvgIcon.js"));
-
-var _default = (0, _createSvgIcon.default)(_react.default.createElement("path", {
-  d: "M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
-}), 'MoreHorizOutlined');
-
-exports.default = _default;
-
-/***/ }),
-
-/***/ "./node_modules/@material-ui/icons/NotificationsOffOutlined.js":
-/*!*********************************************************************!*\
-  !*** ./node_modules/@material-ui/icons/NotificationsOffOutlined.js ***!
-  \*********************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireDefault(__webpack_require__(/*! react */ "react"));
-
-var _createSvgIcon = _interopRequireDefault(__webpack_require__(/*! ./utils/createSvgIcon */ "./node_modules/@material-ui/icons/utils/createSvgIcon.js"));
-
-var _default = (0, _createSvgIcon.default)(_react.default.createElement("path", {
-  d: "M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm0-15.5c2.49 0 4 2.02 4 4.5v.1l2 2V11c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68c-.24.06-.47.15-.69.23l1.64 1.64c.18-.02.36-.05.55-.05zM5.41 3.35L4 4.76l2.81 2.81C6.29 8.57 6 9.74 6 11v5l-2 2v1h14.24l1.74 1.74 1.41-1.41L5.41 3.35zM16 17H8v-6c0-.68.12-1.32.34-1.9L16 16.76V17z"
-}), 'NotificationsOffOutlined');
-
-exports.default = _default;
-
-/***/ }),
-
 /***/ "./node_modules/@material-ui/icons/SubjectOutlined.js":
 /*!************************************************************!*\
   !*** ./node_modules/@material-ui/icons/SubjectOutlined.js ***!
@@ -14194,6 +14100,7 @@ const Row = styled_components_1.default.div `
 const IconContainer = styled_components_1.default.div `
   margin-right: 10px;
   margin-left: 5px;
+  width: 40px;
 `;
 const AvatarContainer = styled_components_1.default.div `
   margin-right: 7px;
@@ -14564,9 +14471,6 @@ const react_1 = __webpack_require__(/*! react */ "react");
 const avatar_1 = __webpack_require__(/*! ../avatar */ "./src/avatar/index.tsx");
 const popup_1 = __webpack_require__(/*! ../popup */ "./src/popup/index.tsx");
 const menu_1 = __webpack_require__(/*! ../menu */ "./src/menu/index.tsx");
-const LockOutlined_1 = __webpack_require__(/*! @material-ui/icons/LockOutlined */ "./node_modules/@material-ui/icons/LockOutlined.js");
-const MoreHorizOutlined_1 = __webpack_require__(/*! @material-ui/icons/MoreHorizOutlined */ "./node_modules/@material-ui/icons/MoreHorizOutlined.js");
-const NotificationsOffOutlined_1 = __webpack_require__(/*! @material-ui/icons/NotificationsOffOutlined */ "./node_modules/@material-ui/icons/NotificationsOffOutlined.js");
 const Container = styled_components_1.default.div `
   background: ${props => props.active ? "#0c1828" : "transparent"};
   display: flex;
@@ -14598,15 +14502,15 @@ const Badge = styled_components_1.default.div `
   font-size: 8px;
   color: white;
   font-weight: 700;
+  margin-left: 5px;
 `;
 const Title = styled_components_1.default.div `
-  overflow: hidden;
   cursor: pointer;
   font-size: 14px;
   font-weight: 500;
   color: ${props => props.active ? "white" : "#475669"};
-  white-space: nowrap;
-  width: max-content;
+  white-space: wrap;
+  max-width: 150px;
   margin-right: 5px;
   font-family: -apple-system, BlinkMacSystemFont,
   "Segoe UI", "Roboto", "Oxygen",
@@ -14651,16 +14555,35 @@ const InnerContents = styled_components_1.default.div `
   justify-content: center;
   position: relative;
 `;
+const MoreMenu = styled_components_1.default.span `
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  z-index: 4;
+  height: 100%;
+  width: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: flex-end;
+  background: linear-gradient(90deg, rgba(2,0,36,0) 0%, rgba(12,24,40,1) 100%);
+`;
 const MoreIcon = styled_components_1.default.span `
   cursor: pointer;
   opacity: 1;
   transition: opacity 0.5s;
   display: inline-block;
-  height: max-content;
-  width: max-content;
-  margin-right: 10px;
-  margin-bottom: -10px;
   z-index: 5;
+  right: 0px;
+  top: 0px;
+  height: 100%;
+  width: 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
 
   &:hover {
     opacity: 0.75;
@@ -14682,24 +14605,28 @@ exports.Room = (props) => {
                 React.createElement(InnerContents, null,
                     React.createElement(Title, { active: props.active || (props.unread != 0) }, props.title),
                     props.muted &&
-                        React.createElement(NotificationsOffOutlined_1.default, { htmlColor: "#475669", fontSize: "small" }),
+                        React.createElement("svg", { width: "20", height: "20", style: { marginRight: 5 }, "aria-hidden": "true", focusable: "false", "data-prefix": "fal", "data-icon": "lock", role: "img", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 640 512" },
+                            React.createElement("path", { fill: "#475669", d: "M320 480c-17.7 0-32-14.4-32-32h-32c0 35.3 28.7 64 64 64s64-28.7 64-64h-32c0 17.6-14.3 32-32 32zm0-416c70.8 0 128 54.5 128 121.9 0 63.2 9.4 101.6 25 129.5l70.7 55.7c.7-13.4-4.2-26.6-14.4-36.3-28-26.6-49.3-54.5-49.3-148.9 0-79.6-63.4-144.5-144-152.3V16c0-8.8-7.2-16-16-16s-16 7.2-16 16v17.6c-43.9 4.2-84.2 26.2-111.5 60.9l24.7 19.4C240.4 83.8 277.7 64 320 64zm317 421.2L23 1.8C19.6-1 14.5-.5 11.8 3l-10 12.5C-1 19-.4 24 3 26.7l614 483.5c3.4 2.8 8.5 2.2 11.2-1.2l10-12.5c2.8-3.5 2.3-8.5-1.2-11.3zM144 384c-14.2 0-21.3-16.5-11.3-26 31.2-29.7 53.9-63 58.3-142.4L159.8 191c-.8 90-21.8 117.7-49.2 143.8-14 13.3-18.4 33.4-11.2 51.2 7.3 18.2 24.8 30 44.6 30h301.6L405 384z" })),
                     !props.public && !props.private &&
-                        React.createElement(LockOutlined_1.default, { htmlColor: "#475669", fontSize: "small" }),
+                        React.createElement("svg", { width: "15", height: "15", "aria-hidden": "true", focusable: "false", "data-prefix": "fal", "data-icon": "lock", role: "img", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 448 512" },
+                            React.createElement("path", { fill: "#475669", d: "M400 224h-16v-62.5C384 73.1 312.9.3 224.5 0 136-.3 64 71.6 64 160v64H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zM96 160c0-70.6 57.4-128 128-128s128 57.4 128 128v64H96v-64zm304 320H48c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h352c8.8 0 16 7.2 16 16v192c0 8.8-7.2 16-16 16z" })),
                     React.createElement(Flex, null),
                     over && props.onMutedClick && props.onArchivedClick &&
-                        React.createElement(popup_1.Popup, { handleDismiss: () => setMenu(false), visible: menu, width: 200, direction: "right-bottom", content: React.createElement(menu_1.Menu, { items: [
-                                    { text: props.archived ? "Unarchive" : "Archive", onClick: (e) => {
-                                            props.onArchivedClick();
-                                        } },
-                                    { text: props.muted ? "Unmute" : "Mute", onClick: (e) => {
-                                            props.onMutedClick();
-                                        } },
-                                ] }) },
-                            React.createElement(MoreIcon, { onClick: (e) => {
-                                    e.stopPropagation();
-                                    setMenu(true);
-                                } },
-                                React.createElement(MoreHorizOutlined_1.default, { htmlColor: "#475669", fontSize: "small" }))),
+                        React.createElement(MoreMenu, null,
+                            React.createElement(popup_1.Popup, { handleDismiss: () => setMenu(false), visible: menu, width: 200, direction: "right-bottom", content: React.createElement(menu_1.Menu, { items: [
+                                        { text: props.archived ? "Unarchive" : "Archive", onClick: (e) => {
+                                                props.onArchivedClick();
+                                            } },
+                                        { text: props.muted ? "Unmute" : "Mute", onClick: (e) => {
+                                                props.onMutedClick();
+                                            } },
+                                    ] }) },
+                                React.createElement(MoreIcon, { onClick: (e) => {
+                                        e.stopPropagation();
+                                        setMenu(true);
+                                    } },
+                                    React.createElement("svg", { width: "15", height: "15", "aria-hidden": "true", focusable: "false", "data-prefix": "fal", "data-icon": "lock", role: "img", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 512 512" },
+                                        React.createElement("path", { fill: "white", d: "M304 256c0 26.5-21.5 48-48 48s-48-21.5-48-48 21.5-48 48-48 48 21.5 48 48zm120-48c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48zm-336 0c-26.5 0-48 21.5-48 48s21.5 48 48 48 48-21.5 48-48-21.5-48-48-48z" }))))),
                     props.unread > 0 &&
                         React.createElement(Badge, null, props.unread)),
                 props.excerpt &&
